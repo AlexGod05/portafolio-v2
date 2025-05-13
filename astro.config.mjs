26946f0333fedf8948d40fc9731dd39a4561d49d
ClaidermanLozano <56901230+AlexGod05@users.noreply.github.com>
2025-05-13 16:51:11 -0500

    Update Astro configuration to change base path to '/portafolio', remove static adapter, and update dependencies in package.json and lock files for improved compatibility and performance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
   output: 'static',
-  adapter: staticAdapter(),
   site: 'https://alexgod05.github.io',
-  base: '/portafolio-v2',
+  base: '/portafolio',
 });
