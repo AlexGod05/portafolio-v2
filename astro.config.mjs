0fe195d9a1402dbe386fb286960ae65222471731
AlexGod05 <lozanocardona200938@gmail.com>
2025-05-13 18:26:14 -0500

    Correct base path in Astro configuration from '/portafolio' to '/portfolio' for accurate asset referencing; update index.html to ensure consistent content structure and improve overall site presentation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://alexgod05.github.io',
-  base: '/portafolio',
+  base: '/portfolio',
   trailingSlash: 'never',
   build: {
     format: 'directory'
