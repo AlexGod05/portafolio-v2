7a405f4fb71ac66ca2443353d9a4d19a33dbe1b5
ClaidermanLozano <56901230+AlexGod05@users.noreply.github.com>
2025-05-13 16:52:36 -0500

    Update base path in Astro config to '/portafolio-v2' for deployment consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://alexgod05.github.io',
-  base: '/portafolio',
+  base: '/portafolio-v2',
 });
