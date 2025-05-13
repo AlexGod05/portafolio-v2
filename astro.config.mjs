a600225e8e655bb48c7b241ee9b25ae0a5420982
ClaidermanLozano <56901230+AlexGod05@users.noreply.github.com>
2025-05-13 16:58:43 -0500

    Enhance Astro configuration by adding trailing slash setting and specifying build format as directory for improved deployment structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ export default defineConfig({
   output: 'static',
   site: 'https://alexgod05.github.io',
   base: '/portafolio-v2',
+  trailingSlash: 'never',
+  build: {
+    format: 'directory'
+  }
 });
